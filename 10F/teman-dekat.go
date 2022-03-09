98c3cf80ac0ee19444bf690dfe09d5db8d268935
Achmad Rizky Syahrani <riskiamad@gmail.com>
2022-03-09 09:15:51 +0700

    update teman-dekat.go

--- 10F/teman-dekat.go
+++ 10F/teman-dekat.go
@@ -5,31 +5,35 @@ import (
 	"math"
 )
 
-var n, x, y, d, min, max float64
+var n, x, y, min, max int64
+var d float64
 
-func f(xi, xj, yi, yj float64) float64 {
-	a := math.Abs(xj - xi)
-	b := math.Abs(yj - yi)
-	return math.Pow(a, d) + math.Pow(b, d)
+func f(xi, xj, yi, yj int64) int64 {
+	a := math.Abs(float64(xj - xi))
+	b := math.Abs(float64(yj - yi))
+	return int64(math.Pow(a, d) + math.Pow(b, d))
 }
 
 func main() {
 	fmt.Scan(&n, &d)
-	var arr = make([][]float64, int(n))
+	var arr = make([][]int64, int(n))
 	for i := 0; i < int(n); i++ {
-		arr[i] = make([]float64, 2)
+		arr[i] = make([]int64, 2)
 		fmt.Scan(&x, &y)
 		arr[i][0] = x
 		arr[i][1] = y
 	}
-	fmt.Print(arr)
-	var data = []float64{}
-	for i := 0; i < int(n); i++ {
-		for j := 0; j < int(n); j++ {
-			if i == j {
-				continue
-			}
-			data = append(data, f(arr[i][0], arr[j][0], arr[i][1], arr[j][1]))
+	i := 0
+	j := 1
+	var data = []int64{}
+	for i < len(arr)-1 {
+		data = append(data, f(arr[i][0], arr[j][0], arr[i][1], arr[j][1]))
+
+		if j < len(arr)-1 {
+			j += 1
+		} else {
+			i += 1
+			j = i + 1
 		}
 
 	}
